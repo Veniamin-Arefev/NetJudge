d14616351ac780c3a29e8910a565d34c97ac64d5
stamplevskiyd <stamplevskiyd@gmail.com>
2022-06-06 12:20:49 +0300

    Changed database location

--- database/__init__.py
+++ database/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 #database_path = 'sqlite:///:memory:'
-database_path = 'sqlite:///' + os.path.abspath('../data.db')
+database_path = 'sqlite:///' + os.path.abspath('./data.db')
 # sqlite:///relative/path/to/file.db
 # sqlite:///:memory: - создать в оперативной памяти
 engine = create_engine(database_path)
